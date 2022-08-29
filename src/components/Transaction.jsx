f5a66618f2976d62210fb6b248b70a001b4b8848
Chibu <atuchibueze@gmail.com>
2022-08-29 07:24:49 -0400

    implement delete transaction

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
   const sign = transaction.amount < 0 ? "-" : "+";
+
   return (
     <div>
       <li className={transaction.amount < 0 ? "minus" : "plus"}>
@@ -9,7 +13,12 @@ export const Transaction = ({ transaction }) => {
         <span>
           {sign}${Math.abs(transaction.amount)}
         </span>
-        <button className="delete-btn">x</button>
+        <button
+          onClick={() => deleteTransaction(transaction.id)}
+          className="delete-btn"
+        >
+          x
+        </button>
       </li>
     </div>
   );
